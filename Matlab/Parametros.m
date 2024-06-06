18ebd5a306263ffdcff34acb08f8b2e2dbb6dffb
Richi <jricardo.21@uc.cl>
2024-06-06 17:45:58 -0400

    Modelo Subido

---
+++ Matlab/Parametros.m
@@ -0,0 +1,12 @@
+% Parametros
+g = 9.81;
+Mw = 0.55;
+Mp = 0.14;
+Jw = 4.36*10^(-3);
+Jp = 0.69*10^(-3);
+L = 0.22;
+Lp = 0.11;
+Jt = Mp*(Lp^(2)) + Mw*(L^(2)) + Jp;
+Mt = (Lp*Mp+L*Mw)*g;
+
+wrapTo2Pi
---
+++ Matlab/TransferFunct.m
@@ -0,0 +1,2 @@
+TF_theta_torque = tf(-1,[Jt 0 -Mt]);
+TF_betha_torque = tf(1, [Jw 0 0]);
