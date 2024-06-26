24c72bca49db4ea44533449b4b156fab0400e685
Richi <jricardo.21@uc.cl>
2024-06-26 00:35:46 -0400

    Final write

--- Matlab/PID+PD/PID_StepResponse.m
+++ Matlab/PID+PD/PID_StepResponse.m
@@ -3,9 +3,9 @@ clc
 planta = tf(-1,[0.029 0 -1.3381]);
 %controller = pid(-5.107*gain,0,-0.48*gain);
 %% Opcion 1 
-%controller = pid(-53.67,-150.657,-4.933);
+controller = pid(-53.67,-15.657,-4.68635);
 %% Opcion 2
-controller = pid(-28.89,-8.427,-2.655);
+%controller = pid(-28.89,-8.427,-2.655);
 sist = series(controller,planta);
 sist_lc=feedback(sist,1);
 stepplot(sist_lc,5)
