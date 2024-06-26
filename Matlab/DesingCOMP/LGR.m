f8111bcea682aedb2a1df2c6a59a245f00c7e512
iangrosan <igross1@uc.cl>
2024-06-25 21:55:22 -0400

    Ahora es PD

--- Matlab/DesingCOMP/LGR.m
+++ Matlab/DesingCOMP/LGR.m
@@ -1,14 +1,12 @@
 % Lead-Lag Controller Parameters
-K = -5.5; % Adjusted gain
-T1 = 0.1; % Lead time constant
-T2 = 0.5; % Lag time constant
+KP = -5.5;
+KD = -0.55;
 planta_num = -1;
 planta_dem = [0.029 0 -1.3381];
 
 planta = tf(planta_num, planta_dem);
 %rlocus(planta)
 
-sist_num = conv(planta_num * K, [1 T1]);
-sist_dem = conv(planta_dem, [1 T2]);
-sist = tf(sist_num, sist_dem);
+sist_num = conv(planta_num, [KD KP]);
+sist = tf(sist_num, planta_dem);
 rlocus(sist);
